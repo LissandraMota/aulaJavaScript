0fd228b658d25ee60e6a1999d029e26082555ee1
lissandramota <lissandramota1991@gmail.com>
2019-10-25 22:08:52 -0300

    aula basica de java

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getHours());
+
+
+/*
+var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while( count <= 5) {
+    console.log(count);
+    count=count + 1;
+};
+*/
+
+
+/*
+var idade= prompt ("Qual a sua idade ?");
+if(idade >= 18){
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+} 
+*/
+
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+//var fruta={nome:"maçã", cor:"vermelha"};
+//console.log(fruta);
+
+
+//var lista = ["banana", "maçã", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join("-"))
+
+
+//var nome="Lissandra Mota";
+//var idade=28;
+//var frase= "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//console.log(frase.replace ("Japão","Brasil"));
+
