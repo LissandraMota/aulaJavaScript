5390aa16b935e6cc80d5e53259a215ec5d9892d2
lissandramota <lissandramota1991@gmail.com>
2019-10-26 12:49:33 -0300

    codigos da aula báica de java

--- js/main.js
+++ js/main.js
@@ -1,9 +1,70 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML= "<b>Obrigada por clicar!</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("obrigada por clicar");
+};
 
+function redirecionar(){
+window.open("https://digitalinnovation.one/sign-in");
+//herf abre a pagina no mesmo ambiente ao redirecionar
+//window.location.href="https://digitalinnovation.one/sign-in";
+};
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML="Obrigada por passar o mouse"
+    //alert("trocar texto");
+};
+function voltar(){
+    document.getElementById("mousemove").innerHTML="passe o mouse aqui"
+};
+
+function load(){
+    alert("página carregada");
+};
+
+function funcaochange(elemento){
+console.log(elemento.value)
+};
+
+
+
+/*
+//esta funcao valida a idade e retorna true or false
+function validaridade(idade){ 
+    if( idade >= 18) { 
+        return true;
+    } else { 
+        return false;
+    }
+};
+
+var idade = prompt ("Qual a sua idade?");
+console.log(validaridade(idade));
+*/
+
+/*
+function soma (n1, n2){
+    return n1+n2;
+};
+*/
+
+//alert(soma (5, 10));
+//alert(setReplace ("vai japão", "japão", "Brasil"));
+
+/*
+//Esta função altera as strings 
+function setReplace (frase, nome, nome_novo){
+    return frase.replace(nome, nome_novo)
+};
+*/
+
+
+/*
 var d = new Date();
 alert(d);
 alert(d.getDay());
 alert(d.getHours());
-
+*/
 
 /*
 var count;
